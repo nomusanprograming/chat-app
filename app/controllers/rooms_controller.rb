dabb521f8c418636ad74f45267032dd443f2af30
nomusanprograming <69701122+nomusanprograming@users.noreply.github.com>
2020-08-25 18:38:15 +0900

    チャット画面の編集

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -15,6 +15,6 @@ class RoomsController < ApplicationController
   private
 
   def room_params
-    params.require(:room).permit(:name, user_isd:[])
+    params.require(:room).permit(:name, user_ids:[])
   end
 end
